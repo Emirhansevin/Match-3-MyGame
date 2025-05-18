d0992d43c800bcf0c8d939e18194f39c01e23c76
Emirhansevin <emirhan_14319@hotmail.com>
2025-05-19 00:23:14 +0300

    v0.4 Code Design Again

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,5 +1,8 @@
+using DG.Tweening;
+using Mono.Cecil;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -16,6 +19,8 @@ public sealed class Board : MonoBehaviour
     public int Height => Tiles.GetLength(1);
 
     private readonly List<Tile> _selection = new List<Tile>();
+
+    private const float TweenDuration = 0.25f;
 
     private void Awake()
     {
@@ -37,13 +42,19 @@ public sealed class Board : MonoBehaviour
                 Tiles[x, y] = tile;
             }
         }
+    }
 
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.A)) return;
 
-
-
+        foreach (var connectedTile in Tiles[0, 0].GetConnectedTiles()) connectedTile.icon.transform.DOScale(1.25f, TweenDuration).Play();
+        {
+            
+        }
 
     }
-    public void Select(Tile tile)
+    public  async void Select(Tile tile)
     {
         if (!_selection.Contains(tile)) _selection.Add(tile);
        
@@ -51,10 +62,47 @@ public sealed class Board : MonoBehaviour
 
         Debug.Log($"Selected tiles at ({_selection[0].x} , {_selection[0].y} and {_selection[1].x} , {_selection[1].y}");
 
+        await Swap(_selection[0], _selection[1]);
+
         _selection.Clear();
 
+    }
+    public async Task Swap(Tile tile1, Tile tile2)
+    {
+        var icon1 =tile1.icon;
+        var icon2 =tile2.icon;
+
+        var icon1Transform = icon1.transform;
+        var icon2Transform = icon2.transform;
+
+        var sequence = DOTween.Sequence();
+
+        sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenDuration))
+            .Join(icon2Transform.DOMove(icon1Transform.position, TweenDuration));
 
 
+        
+        await sequence.Play().AsyncWaitForCompletion();
+
+        icon1Transform.SetParent(tile2.transform);
+        icon2Transform.SetParent(tile1.transform);
+
+        tile1.icon = icon2;
+        tile2.icon = icon1;
+
+        var tileItem = tile1.Item;
+
+        tile1.Item = tile2.Item;
+        tile2.Item = tileItem;
+
+    }
+
+    private bool CanPop()
+    {
+
+    }
+    private void Pop()
+    {
 
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -26,9 +29,45 @@ public sealed class Tile : MonoBehaviour
 
     public Button button;
 
+    public Tile Left => x > 0 ? Board.Instance.Tiles[x - 1, y] : null;
+    public Tile Top => y < 0 ? Board.Instance.Tiles[x ,y] : null;
+    public Tile Right => x < Board.Instance.Width - 1 ? Board.Instance.Tiles[x + 1, y] : null;
+    public Tile Bottom => y < Board.Instance.Width - 1 ? Board.Instance.Tiles[x, y + 1] : null;
+
+    public Tile[] Neighbours => new[]
+    {
+        Left,
+        Top,
+        Right,
+        Bottom
+    };
+
     private void Start()
     {
         button.onClick.AddListener(() => Board.Instance.Select(this));
+    }
+
+    public List<Tile> GetConnectedTiles(List<Tile> exclude = null)
+    {
+        var result = new List<Tile> { this, };
+
+        if (exclude == null)
+        {
+            exclude = new List<Tile> { this, };
+        }
+        else
+        {
+            exclude.Add(this);
+        }
+
+        foreach (var Neighbour in Neighbours)
+        {
+            if (Neighbour == null || exclude.Contains(Neighbour) || Neighbour.Item != Item) continue;
+
+            result.AddRange(Neighbour.GetConnectedTiles(exclude));
+        }
+
+        return result;
     }
 
 }
